fa6dfc6d769a783f696c7e3b908c21407da4e524
PiskaThu <70908420+PiskaThu@users.noreply.github.com>
2026-06-01 01:44:09 -0300

    add command ping

--- bot.py
+++ bot.py
@@ -9,6 +9,8 @@ TOKEN = os.getenv("DISCORD_TOKEN")
 CANAL_ID = int(os.getenv("CANAL_ID"))
 
 intents = discord.Intents.default()
+intents.message_content = True  # necessário para comandos
+
 bot = commands.Bot(command_prefix="!", intents=intents)
 
 @bot.event
@@ -18,7 +20,13 @@ async def on_ready():
     canal = bot.get_channel(CANAL_ID)
     if canal:
         await canal.send("Bot online 🚀")
-    else:
-        print("Canal não encontrado.")
+
+
+# Comando !ping
+@bot.command()
+async def ping(ctx):
+    latency = round(bot.latency * 1000)
+    await ctx.send(f'🏓 Pong! {latency}ms')
+
 
 bot.run(TOKEN)
